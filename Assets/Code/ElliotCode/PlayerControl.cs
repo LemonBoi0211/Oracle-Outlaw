58c2b1e6ef6873564f1820418b91261b18cc4bb6
LemonBoi0211 <lemonboi0211@outlook.com>
2023-09-28 14:53:15 +0100

    player orbits, need to add input to moveorbits

--- Assets/Code/ElliotCode/PlayerControl.cs
+++ Assets/Code/ElliotCode/PlayerControl.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public GameObject smallOrbitP;
+    public GameObject bigOrbitP;
+    public float velocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        velocity = 50f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.RotateAround(smallOrbitP.transform.localPosition, Vector3.forward, Time.deltaTime * velocity);
     }
 }
