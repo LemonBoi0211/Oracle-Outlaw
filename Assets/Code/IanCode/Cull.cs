fd6e7f6832c02bc0f273592fd3a1cd4d96da4f28
jack howlett <145658104+ian427@users.noreply.github.com>
2023-09-28 15:48:05 +0100

    danger spawn
    danger spawn and cull

---
+++ Assets/Code/IanCode/Cull.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cull : MonoBehaviour
+{
+    Camera cam;
+    public float Height;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        ScreenSize();
+        Debug.Log("Spawned");
+    }
+    void ScreenSize()
+    {
+        
+        Height = 1 / (cam.WorldToViewportPoint(new Vector3(1, 1, 0)).y - .5f);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        Vector3 randomSP = new Vector3(0,(Height / 2) / -1 - (1) , 0);
+        transform.position = randomSP;
+
+    }
+   /* void OnCollisionEnter(Collision collision)
+    {
+       
+    }*/
+
+    public void OnTriggerEnter2D (Collider2D collision)
+    {
+         Debug.Log("hit");
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Dead");
+            Destroy(collision.gameObject);
+        }
+    }
+}
