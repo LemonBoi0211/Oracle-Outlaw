39c018e8ae56d4bafb764f50435b8297e796ddba
LemonBoi0211 <lemonboi0211@outlook.com>
2023-10-12 14:29:18 +0100

    fortune generation

---
+++ Assets/Code/ElliotCode/FortuneGen.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FortuneGen : MonoBehaviour
+{
+    public WordArray wordArray;
+    public TMP_Text fortuneTold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string fortuneGen = wordArray.wordAdj[Random.Range(0, wordArray.wordAdj.Length)] + " " + wordArray.wordnoun[Random.Range(0, wordArray.wordnoun.Length)] + " " + wordArray.wordverb[Random.Range(0, wordArray.wordverb.Length)] + " " + wordArray.wordAdj[Random.Range(0, wordArray.wordAdj.Length)] + " " + wordArray.wordnoun[Random.Range(0, wordArray.wordnoun.Length)];
+
+        fortuneTold.text = fortuneGen;
+
+
+        //wordArray.wordverb[];
+        //wordArray.wordnoun[];
+        //wordArray.wordAdj[];
+    }
+}
